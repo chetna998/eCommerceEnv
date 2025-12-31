a56ef64f9b84bc12cc0b9c15eba5ef68986b3ed2
chetna998 <chetna.salhan098@gmail.com>
2025-12-31 08:38:08 +0530

    integrated tailwind and angular material

--- ecommerceApp/src/app/app.routes.ts
+++ ecommerceApp/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        loadComponent: () => import('./features/products/product-list/product-list')
+            .then(c => c.ProductList)
+    }
+];
---
+++ ecommerceApp/src/app/features/products/product-list/product-list.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [MatButtonModule],
+  templateUrl: './product-list.html',
+  styleUrl: './product-list.scss',
+})
+export class ProductList {
+
+}
