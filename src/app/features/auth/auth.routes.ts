3187e1e30e22e40ef7d0858022fffed387c7c263
chetna998 <chetna.salhan098@gmail.com>
2026-01-06 00:55:27 +0530

    implemented lazy loading, guards, pending - interceptors

---
+++ src/app/features/auth/auth.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { Login } from "./login/login";
+
+export const Auth_Routes:Routes = [
+    {
+        path: 'login',
+        component: Login
+    }
+]
---
+++ src/app/features/auth/login/login.ts
@@ -0,0 +1,40 @@
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth-service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [MatButtonModule, MatCardModule, MatInputModule, MatFormFieldModule, FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+  email: string = '';
+  password: string = '';
+  isLoading = false;
+
+  private authService = inject(AuthService)
+  private router = inject(Router);
+
+  handleSubmit() {
+    if (!this.email || !this.password) return;
+    this.isLoading = true;
+    //set temp loggedIn flag
+    this.authService.login({ email: this.email, password: this.password }).subscribe({
+      next: (success) => {
+        this.isLoading = false
+        if(success) {
+          this.router.navigate(['/user'])
+        }
+      },
+      error: () => {
+        this.isLoading = false;
+      }
+    });
+  }
+}
