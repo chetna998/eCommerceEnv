27653e08535e41e3848d0d85d2b18f2c280578a6
chetna998 <chetna.salhan098@gmail.com>
2026-01-06 00:47:54 +0530

    implemented lazy loading, guards, pending - interceptors

---
+++ src/app/features/products/product.routes.ts
@@ -0,0 +1,8 @@
+import { ProductList } from "./product-list/product-list";
+
+export const Product_Routes = [
+    {
+        path:'',
+        component: ProductList
+    }
+]
---
+++ src/app/features/user/user.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { Profile } from "./profile/profile";
+
+export const User_Routes:Routes = [
+    {
+        path:'',
+        component: Profile
+    }
+]
