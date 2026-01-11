99f570d090703bf99f2ff9aa9132532511b64ff9
chetna998 <66053872+chetna998@users.noreply.github.com>
2026-01-12 01:22:44 +0530

    interceptor implemented (#2)
    * added project initial files
    
    * integrated tailwind and angular material
    
    * fixed structure
    
    * migrated to 21
    
    * implemented lazy loading, guards, pending - interceptors
    
    * implemented lazy loading, guards, pending - interceptors
    
    * reverted to CSR and implemented interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,17 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { dummyAuthInterceptorInterceptor } from './core/interceptors/dummy-auth-interceptor-interceptor';
+import { loadingInterceptor } from './core/interceptors/loading-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+     provideHttpClient(
+      withInterceptors([dummyAuthInterceptorInterceptor, loadingInterceptor])
+    )
   ]
 };
