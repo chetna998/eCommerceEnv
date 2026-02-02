3200fc6a7ca1cf7f925ba9a77c67fee508754b3a
chetna998 <chetna.salhan098@gmail.com>
2026-02-02 23:15:32 +0530

    upgrade to angular 21

--- frontend/src/app/core/services/product-service.ts
+++ frontend/src/app/core/services/product-service.ts
@@ -8,7 +8,7 @@ import { Product } from '../interfaces/product.interface';
   providedIn: 'root',
 })
 export class ProductService {
- private baseurl = `${environment.apiBaseUrl}/product4s`;
+ private baseurl = `${environment.apiBaseUrl}/products`;
  private http = inject(HttpClient)
 
  getProducts():Observable<Product[]>{
